2252cad1ed9c50d94bf970d41d896b7a3ff0c08a
Kristijan20 <kikomaksimovski2002@gmail.com>
2024-01-22 18:12:25 +0100

    addet funcion for urls

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
 
-const PORT = 8000
+server.get("/ourTeam/:id", (req, res, next) => {
+  const teamId = parseInt(req.params.id, 10);
+  const member = router.db.get("ourTeam").find({ id: teamId }).value();
+
+  if (member) {
+    res.jsonp(member);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+server.use(router);
+
+const PORT = 8000;
 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
