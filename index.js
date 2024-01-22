4b823ea5ed2350b95db115422049ec755510062c
Kristijan20 <kikomaksimovski2002@gmail.com>
2024-01-22 18:14:58 +0100

    .

--- index.js
+++ index.js
@@ -9,18 +9,6 @@ const middlewares = jsonServer.defaults();
 server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
-
-server.get("/ourTeam/:id", (req, res, next) => {
-  const teamId = parseInt(req.params.id, 10);
-  const member = router.db.get("ourTeam").find({ id: teamId }).value();
-
-  if (member) {
-    res.jsonp(member);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
 server.use(router);
 
 const PORT = 8000;
