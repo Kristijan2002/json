567e25c96f17201cc66ee91f674b7739b3547fb3
Kristijan20 <kikomaksimovski2002@gmail.com>
2024-01-26 12:05:34 +0100

    Change configuration

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
+server.options("*", cors()); // Enable CORS for all routes
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
+
+// Async function as middleware
+server.use(async (req, res, next) => {
+  // Your custom logic here, if needed
+  await next();
+});
+
 server.use(router);
 
 const PORT = 8000;
